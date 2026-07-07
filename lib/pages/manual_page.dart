8d5bbd54216440977d6810b2288e5bd0301aa275
Dean <deansylvester7@gmail.com>
2026-07-07 15:08:03 -0500

    update website

--- lib/pages/manual_page.dart
+++ lib/pages/manual_page.dart
@@ -1,18 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:pdfx/pdfx.dart';
+import 'package:http/http.dart' as http;
 
-class ManualPage extends StatelessWidget {
+
+class ManualPage extends StatefulWidget {
   const ManualPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "HSE Manual",
-        style: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.bold,
-        ),
+  State<ManualPage> createState() => _ManualPageState();
+}
+
+
+class _ManualPageState extends State<ManualPage> {
+
+  PdfControllerPinch? pdfController;
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadPdf();
+  }
+
+
+  Future<void> loadPdf() async {
+
+    final response = await http.get(
+      Uri.parse(
+        "https://safebot-backend.onrender.com/manual",
       ),
+    );
+
+
+    final document = await PdfDocument.openData(
+      response.bodyBytes,
+    );
+
+
+    setState(() {
+      pdfController = PdfControllerPinch(
+        document: Future.value(document),
+      );
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text("HSE Manual"),
+      ),
+
+
+      body: pdfController == null
+
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+
+          : PdfViewPinch(
+              controller: pdfController!,
+            ),
     );
   }
 }
