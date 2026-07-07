c0606fecce19217e415f2a5663a2c127c7e39321
Dean <deansylvester7@gmail.com>
2026-07-07 15:56:14 -0500

    update website

--- lib/pages/manual_page.dart
+++ lib/pages/manual_page.dart
@@ -1,39 +1,20 @@
-import 'dart:html' as html;
-import 'dart:ui_web' as ui_web;
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class ManualPage extends StatelessWidget {
   const ManualPage({super.key});
 
+  final String manualUrl =
+      "https://safebot-backend.onrender.com/manual";
+
   @override
   Widget build(BuildContext context) {
-
-    const String pdfUrl =
-        "https://safebot-backend.onrender.com/manual";
-
-    final String viewId = "manual-pdf-viewer";
-
-    ui_web.platformViewRegistry.registerViewFactory(
-      viewId,
-      (int viewId) {
-        final iframe = html.IFrameElement()
-          ..src = pdfUrl
-          ..style.border = 'none'
-          ..style.width = '100%'
-          ..style.height = '100%';
-
-        return iframe;
-      },
-    );
-
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("HSE Manual"),
       ),
-
-      body: HtmlElementView(
-        viewType: viewId,
+      body: SfPdfViewer.network(
+        manualUrl,
       ),
     );
   }
