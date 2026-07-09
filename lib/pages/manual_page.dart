f842626e96085c8d1ede89874a6a3d5d03bcd833
Dean <deansylvester7@gmail.com>
2026-07-09 10:09:42 -0500

    test manual page

--- lib/pages/manual_page.dart
+++ lib/pages/manual_page.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class ManualPage extends StatelessWidget {
   const ManualPage({super.key});
 
-  final String manualUrl =
-      "https://safebot-backend.onrender.com/manual";
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("HSE Manual"),
-      ),
-      body: SfPdfViewer.network(
-        manualUrl,
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Manual Page Works",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
